a6ca8040e648530708c1f1f24653b50baa1ff1e7
Jandemeskel95 <trainee.john.andemeskel@sigmalabs.co.uk>
2022-08-22 11:38:40 +0100

    Scrabble test start - 11:37am

---
+++ scrabble/Scrabble.py
@@ -0,0 +1,4 @@
+
+alphabet = [chr(i).upper() for i in range(ord('a'),ord('z')+1)]
+points = [1,2,3,4,5,8,10]
+letter_value_groups = [['E', 'A', 'I', 'O','N', 'R', 'T', 'L', 'S', 'U'], ['D', 'G'], ['B', 'C', 'M', 'P'], ['F', 'H', 'V', 'W', 'Y'], ['K'], ['J', 'X'], ['Q', 'Z']]
