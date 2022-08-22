b90577d369f4a309d25dfcf55550a12dadd9fa7f
Jandemeskel95 <trainee.john.andemeskel@sigmalabs.co.uk>
2022-08-22 12:19:59 +0100

    Scrabble task 2 & 3

--- scrabble/Scrabble.py
+++ scrabble/Scrabble.py
@@ -1,5 +1,6 @@
 
-alphabet = [chr(i).upper() for i in range(ord('a'),ord('z')+1)]
+from random import shuffle
+
 points = [1,2,3,4,5,8,10]
 letter_value_groups = [['E', 'A', 'I', 'O','N', 'R', 'T', 'L', 'S', 'U'], ['D', 'G'], ['B', 'C', 'M', 'P'], ['F', 'H', 'V', 'W', 'Y'], ['K'], ['J', 'X'], ['Q', 'Z']]
 number_of_tiles = [12,9,8,6,4,3,2,1]
@@ -34,6 +35,34 @@ def calculate_word_score(input_word, letter_scores):
 
     return score
 
+
+def generate_bag(letter_distribution):
+
+    bag = []
+    for key, value in letter_distribution.items():
+        letter_tiles = [*key*value]
+        bag.append(letter_tiles)
+
+    return sum(bag,[])
+
+def deal_tiles(bag):
+    
+    player_hand = []
+    while len(player_hand)<=8:
+        player_hand.append(bag.pop())
+    
+    return player_hand
+
+
 # Task 1
 letter_values = letters_to_points(letter_value_groups)
 guardian_score = calculate_word_score('GUARDIAN', letter_values)
+
+# Task 2 & 3
+
+letter_distribution = get_letter_distribution(letter_distribution_groups)
+bag = generate_bag(letter_distribution)
+shuffle(bag)
+player_hand = deal_tiles(bag)
+print("\n Your Hand is:")
+print(* player_hand, sep=', ')
