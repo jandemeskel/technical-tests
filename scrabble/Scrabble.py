591f7f9aedbb3ae991abaf129eecb7d71026a03c
Jandemeskel95 <trainee.john.andemeskel@sigmalabs.co.uk>
2022-08-25 15:52:20 +0100

    Scrabble search algorth fixed - task 5 complete

--- scrabble/Scrabble.py
+++ scrabble/Scrabble.py
@@ -1,11 +1,11 @@
-
 from random import shuffle
+from sys import exit
 
+alphabet = [chr(i).upper() for i in range(ord('a'),ord('z')+1)]
 points = [1,2,3,4,5,8,10]
 letter_value_groups = [['E', 'A', 'I', 'O','N', 'R', 'T', 'L', 'S', 'U'], ['D', 'G'], ['B', 'C', 'M', 'P'], ['F', 'H', 'V', 'W', 'Y'], ['K'], ['J', 'X'], ['Q', 'Z']]
 number_of_tiles = [12,9,8,6,4,3,2,1]
 letter_distribution_groups = [['E'], ['A','I'], ['O'], ['N','R','T'], ['L','S','U','D'], ['G'], ['B', 'C', 'M', 'P', 'F', 'H', 'V', 'W', 'Y'], ['K', 'J', 'X', 'Q', 'Z']]
-
 
 def letters_to_points(letter_value_groups):
     letters_to_points = {}
@@ -48,11 +48,10 @@ def generate_bag(letter_distribution):
 def deal_tiles(bag):
     
     player_hand = []
-    while len(player_hand)<=8:
+    while len(player_hand)<=7:
         player_hand.append(bag.pop())
     
     return player_hand
-
 
 def valid_player_input(player_word, player_hand):
 
@@ -64,10 +63,9 @@ def valid_player_input(player_word, player_hand):
         return True
     
 
-
 def check_dictionary(player_word, word_score):
 
-    dictionary = open("technical-tests/scrabble/dictionary.txt", "r")
+    dictionary = open("dictionary.txt", "r")
     if player_word.lower() in dictionary.read():
         print(f' \n Your input is a valid word found in the dictionary, scoring {word_score} point')
         return True
@@ -75,34 +73,41 @@ def check_dictionary(player_word, word_score):
         print('Invalid word not found in the dictionary')
         return False
 
+
 def find_longest_word(player_hand):
 
     longest_word = ''
 
-    dictionary = open("technical-tests/scrabble/dictionary.txt", "r")
-    words = dictionary.readlines()
+    f = open("dictionary.txt")
+    words = f.readlines()
     
     for word in words:
         if len(longest_word) < len(word):
             
-            current_dictionary_word = [*word]
-            current_player_hand = player_hand
-    
-            for letter in current_dictionary_word:
+            dictionary_word = [*word]
+            current_dictionary_word = dictionary_word.copy()
+            current_player_hand = player_hand.copy()
+
+            for letter in dictionary_word:
                 if letter.upper() in current_player_hand:
 
                     current_dictionary_word.remove(letter)
                     current_player_hand.remove(letter.upper())
-
+                else:
+                    break
+                
             current_dictionary_word.remove('\n')
             
             if len(current_dictionary_word) == 0:
                 longest_word = word
+
     print(f"The longest word with the current player hand is {len(longest_word)} letters long, for instance {longest_word}")
     return longest_word
+        
+        
+
 
 # Task 1
-
 letter_values = letters_to_points(letter_value_groups)
 guardian_score = calculate_word_score('GUARDIAN', letter_values)
 
@@ -123,6 +128,7 @@ while flag:
 
     if player_word == '0':
         exit()
+
     elif valid_player_input(player_word, player_hand):
         flag = False
 
@@ -132,10 +138,6 @@ is_valid_word = check_dictionary(player_word, word_score)
 
 # Task 5
 longest_word = find_longest_word(player_hand)
-# Trying to debug, not sure why current_player_hand is not being reassigned back to player_hand
-# both are empty but should not be, every iteration should reassign
-
-
 # Improvements #
 
 
@@ -152,3 +154,7 @@ longest_word = find_longest_word(player_hand)
 # what type of search did i implement? might look at all words but it doesn't check them all so is it linear?
 # search moves on from word as soon as letter not in hand
 # also skips words which are not longer than current longest word
+
+
+
+
